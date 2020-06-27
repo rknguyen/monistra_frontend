6b6d7732624c57fda3cd8ae2657c16a69f976575
Huy Nguyen <huy.nguyen@trustingsocial.com>
2020-06-28 03:01:12 +0700

    Add draw polygon

--- src/views/Editor/Editor.ts
+++ src/views/Editor/Editor.ts
@@ -6,11 +6,13 @@ import { RectangleService } from './services/Rectangle'
 import { Layer } from 'konva/types/Layer'
 import { Stage } from 'konva/types/Stage'
 import { EllipseService } from './services/Ellipse'
+import { Vector2d } from 'konva/types/types'
 
 enum Mode {
   Select = 0,
   DrawRectangle = 1,
-  DrawEllipse = 2
+  DrawEllipse = 2,
+  DrawPolygon = 3
 }
 
 type Service = RectangleService | EllipseService
@@ -25,6 +27,12 @@ export default class Editor extends Vue {
   private Services: Service[] = []
 
   public editorMode = Mode.Select
+
+  private isDrawingPolygon = false
+  private lines: Konva.Line[] = []
+  private points: Konva.Rect[] = []
+  private groups: Konva.Group[] = []
+  private isFinishPolygon = false
 
   get topService() {
     return this.Services[this.Services.length - 1]
@@ -88,6 +96,19 @@ export default class Editor extends Vue {
       if (this.isDrawingMode(this.editorMode) && this.isDrawing) {
         this.topService.onMouseMove(this.stage as Stage, this.layer as Layer, event)
       }
+      if (this.editorMode === Mode.DrawPolygon && this.isDrawingPolygon) {
+        const cursor = (this.stage as Stage).getPointerPosition() as Vector2d
+        const line = this.lines[this.lines.length - 1] as Konva.Line
+        if (line.zIndex() > 0) {
+          this.points[0].zIndex(line.zIndex())
+        }
+        line.zIndex(0)
+        const points = line.points()
+        points[2] = cursor.x
+        points[3] = cursor.y
+        line.points(points)
+        this.layer?.draw()
+      }
     })
     this.stage?.on('mouseup', (event: KonvaEventObject<MouseEvent>) => {
       if (this.isDrawingMode(this.editorMode) && this.isDrawing) {
@@ -95,6 +116,62 @@ export default class Editor extends Vue {
         this.topService.onMouseUp(this.stage as Stage, this.layer as Layer, event)
         if (this.topService.isDestroyed) {
           this.Services.pop()
+        }
+      }
+    })
+    this.stage?.on('click', (event: KonvaEventObject<MouseEvent>) => {
+      if (this.editorMode === Mode.DrawPolygon) {
+        const cursor = (this.stage as Stage).getPointerPosition() as Vector2d
+
+        let isStartingPoint = false
+        if (this.isDrawingPolygon) {
+          const line = this.lines[this.lines.length - 1] as Konva.Line
+          const points = line.points()
+          points[2] = cursor.x
+          points[3] = cursor.y
+          line.points(points)
+          this.layer?.draw()
+        } else {
+          if (!this.isFinishPolygon) {
+            this.isDrawingPolygon = true
+            isStartingPoint = true
+            this.groups.push(new Konva.Group())
+          }
+        }
+
+        if (this.isFinishPolygon) {
+          this.isFinishPolygon = false;
+        } else {
+          const group = this.groups[this.groups.length - 1]
+          const popts = { x: cursor.x - 3, y: cursor.y - 3, width: 6, height: 6, stroke: 'red', fill: 'red' }
+          this.points.push(new Konva.Rect(popts))
+          const point = this.points[this.points.length - 1]
+          group.add(point)
+          if (isStartingPoint) {
+            point.hitStrokeWidth(12)
+            point.on('mouseover', (event: KonvaEventObject<MouseEvent>) => {
+              if (this.points.length >= 3) {
+                point.scale({ x: 2, y: 2 })
+              }
+            })
+            point.on('mouseout', (event: KonvaEventObject<MouseEvent>) => {
+              point.scale({ x: 1, y: 1 })
+            })
+            point.on('click', (event: KonvaEventObject<MouseEvent>) => {
+              if (this.points.length >= 3) {
+                this.isDrawingPolygon = false
+                this.points = []
+                event.evt.preventDefault()
+                this.isFinishPolygon = true
+              }
+            })
+          }
+          const line = new Konva.Line({ points: [cursor.x, cursor.y, cursor.x, cursor.y], stroke: 'black', strokeWidth: 5 })
+          group.add(line)
+          this.lines.push(line)
+          this.layer?.add(group)
+          this.stage?.add(this.layer)
+          this.layer?.draw()
         }
       }
     })
--- src/views/Editor/Editor.vue
+++ src/views/Editor/Editor.vue
@@ -4,6 +4,7 @@
       <button :class="{ active: editorMode === 0 }" @click="editorMode = 0">Select</button>
       <button :class="{ active: editorMode === 1 }" @click="editorMode = 1">Rectangle</button>
       <button :class="{ active: editorMode === 2 }" @click="editorMode = 2">Ellipse</button>
+      <button :class="{ active: editorMode === 3 }" @click="editorMode = 3">Polygon</button>
     </div>
     <div id="konva-stage"></div>
   </div>
